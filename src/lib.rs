3a480c438714bbf36bd5d07551a3eb482f662822
Dylan Brown <deejaybee11@gmail.com>
2025-10-20 07:56:22 +0100

    small edit

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+using ndarray::{ArrayD, Array2, Array1, arr1, arr2};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+let const SAFETY = 0.9;
+let const MIN_FACTOR = 0.2;
+let const MAX_FACTOR = 10;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn rk_step(f: impl Fn(f64, &[f64]) -> Vec<f64>, t: f64, y: &[f64], h: f64, A: Array2, B: Array1, C: Array1, mut K: Array2) -> (Vec<f64>, Vec<f64>) {
+
+    k[0]
+
 }
